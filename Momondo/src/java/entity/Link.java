00f5c1427f605eb8f06ea00a60c19645a5a53ad5
Sindt <czindt@gmail.com>
2015-12-01 19:10:10 +0100

    Link facade added

---
+++ Momondo/src/java/entity/Link.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Sindt
+ */
+@Entity
+@Table(name = "link")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Link.findAll", query = "SELECT l FROM Link l"),
+    @NamedQuery(name = "Link.findByLinkId", query = "SELECT l FROM Link l WHERE l.linkId = :linkId"),
+    @NamedQuery(name = "Link.findByUrl", query = "SELECT l FROM Link l WHERE l.url = :url")})
+public class Link implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "link_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long linkId;
+    @Size(max = 255)
+    @Column(name = "url")
+    private String url;
+
+    public Link() {
+    }
+
+    public Link(Long linkId) {
+        this.linkId = linkId;
+    }
+
+    public Long getLinkId() {
+        return linkId;
+    }
+
+    public void setLinkId(Long linkId) {
+        this.linkId = linkId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (linkId != null ? linkId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Link)) {
+            return false;
+        }
+        Link other = (Link) object;
+        if ((this.linkId == null && other.linkId != null) || (this.linkId != null && !this.linkId.equals(other.linkId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Link[ linkId=" + linkId + " ]";
+    }
+    
+}
---
+++ Momondo/src/java/facades/LinkFacade.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import deploy.DeploymentConfiguration;
+import entity.Link;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Sindt
+ */
+public class LinkFacade {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(DeploymentConfiguration.PU_NAME);
+
+    public LinkFacade() {
+    }
+
+    public Link addLink(Link l) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(l);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+        } finally {
+            em.close();
+        }
+
+        return l;
+    }
+
+    public Link getLink(int id) {
+        Link l = null;
+        EntityManager em = emf.createEntityManager();
+        try {
+            l = em.find(Link.class, id);
+        } catch (Exception e) {
+        } finally {
+            em.close();
+        }
+        return l;
+    }
+
+    public List<Link> getAllLinks() {
+        EntityManager em = emf.createEntityManager();
+        Query q;
+        try {
+            q = em.createNamedQuery("Link.findAll", Link.class);
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+}
