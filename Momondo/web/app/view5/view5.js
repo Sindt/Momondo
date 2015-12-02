da28ffb9292c42e8ecaf436cd031fbb2cb53a4e3
KasperOlesen <kasperaolesen@gmail.com>
2015-12-02 10:45:32 +0100

    Adding Angular/REST to search function

---
+++ Momondo/web/app/view5/view5.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('myApp.view5', ['ngRoute'])
+
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5', {
+                    templateUrl: 'app/view5/view5.html'
+                });
+            }])
+        .controller("View5Ctrl", ["$http", "$scope", function ($http, $scope) {
+                var self = this;
+                $scope.getAllFlightinfo = function () {
+                    $http({
+                        method: "GET",
+                        url: "api/flightinfo/:from/:date/:numTickets"
+                    }).then(function succesCallback(response) {
+                        self.data = response.data;
+                    }).then(function errorCallback(response) {
+                        self.error = "Error";
+                    });
+                };
+                $scope.getAllFlightInfo();
+
+//                $scope.deleteUser = function (users) {
+//                    $http({
+//                        method: "DELETE",
+//                        url: "api/admin/user/" + users.id
+//                    }).then(function succesCallback(response) {
+//                        $scope.getUsers();
+//
+//                    });
+//
+//
+//                };
+            }]);
