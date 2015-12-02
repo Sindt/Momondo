32813faccd94333e2ecd68bdb36ee7b459552f21
KasperOlesen <kasperaolesen@gmail.com>
2015-12-02 18:52:20 +0100

    Updated flight search (not complete) Updated registration (not complete)

--- Momondo/web/app/view6/view6.js
+++ Momondo/web/app/view6/view6.js
@@ -2,8 +2,29 @@
 
 angular.module('myApp.view6', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view6', {
-    templateUrl: 'app/view6/view6.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view6', {
+                    templateUrl: 'app/view6/view6.html'
+                });
+
+            }])
+        .controller("View6Controller", ['$scope', '$http', function ($scope, $http) {
+
+                $scope.addRegistrationAsJSON = function () {
+                  
+                    var res = $http.post('api/flightreservation', $scope.registration);
+                    res.success(function (data, status, headers, config) {
+                        $scope.message = data;
+                        alert("Flight registered. Have a nice trip.");
+                        
+                        
+                        
+                        
+//                        $scope..username = '';
+//                        $scope.newuser.password = '';
+                    });
+                    res.error(function (data, status, headers, config) {
+                        alert("failure message: " + JSON.stringify({data: data}));
+                    });
+                };
+            }]);
