60628fb995336d3764f7387b81433096cfd0b1d3
KasperOlesen <kasperaolesen@gmail.com>
2015-12-15 17:19:00 +0100

    - Front-end user registration done

--- Momondo/web/app/view7/view7.js
+++ Momondo/web/app/view7/view7.js
@@ -1,15 +1,30 @@
 'use strict';
+
 angular.module('myApp.view7', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
                 $routeProvider.when('/view7', {
                     templateUrl: 'app/view7/view7.html'
                 });
-
             }])
-        .controller("View7Ctrl", ["$scope", "dataFactory", function ($scope, dataFactory) {
-                
-                
-                
-                
-        }]);
+
+        .controller("View7Ctrl", ['$scope', '$http', function ($scope, $http) {
+
+                $scope.addUserAsJSON = function () {
+                    // Writing it to the server
+                    //		
+
+                    var res = $http.post('api/user/registration', $scope.newuser);
+                    res.success(function (data, status, headers, config) {
+                        $scope.message = data;
+                        alert("You are registered. Go to the Login Section to log in.");
+                        $scope.newuser.username = '';
+                        $scope.newuser.password = '';
+                    });
+                    res.error(function (data, status, headers, config) {
+                        alert("failure message: " + JSON.stringify({data: data}));
+                    });
+                };
+            }]);
+
+         
